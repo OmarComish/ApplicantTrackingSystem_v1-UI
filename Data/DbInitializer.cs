0b92bee11dcec6ace361fe058cd162fc3eb6f444
OmarComish <omarthommy@gmail.com>
2026-02-24 18:01:42 +0200

    Resolved the appsettings file

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -19,7 +19,7 @@ public class DbInitializer
             Console.WriteLine("Seeding Users data initiated...");
             var _users = new List<User>
             {
-                new() {FirstName ="Mariam", LastName="Kasupe", Email ="mariekasupe@gmail.com", 
+                new() {Id = 1, FirstName ="Mariam", LastName="Kasupe", Email ="mariekasupe@gmail.com", 
                 IsActive = true, CreatedAt = DateTime.UtcNow, CreatedBy ="system", Role = UserRole.HRAdmin}
             };
             context.AddRange(_users);
